bacf95c65dc005049ec568e2a472e7d2e5a0ef15
helenewegener1 <helene.wegener@gmail.com>
2026-01-17 10:57:18 +0100

    07 QC version

---
+++ 07_seurat_QC/script/merged_clusters.R
@@ -0,0 +1,223 @@
+merged_clusters_all <- list(
+  
+  "v9" = list(
+    "HH117-SILP-INF-PC" = list( 
+      # new_cluster = c(old clusters)
+      "0" = c("0", "2", "3", "4"),
+      "1" = c("1"),
+      "2" = c("5")
+    ),
+    
+    "HH117-SILP-nonINF-PC" = list(
+      # new_cluster = c(old clusters)
+      "0" = c("0", "2"),
+      "1" = c("1", "3")
+    ),
+    
+    "HH117-SI-MILF-INF-HLADR-AND-CD19" = list(
+      # new_cluster = c(old clusters)
+      "0" = c("0", "2", "3"),
+      "1" = c("1"),
+      "2" = c("4"),
+      "3" = c("5")
+    ),
+    
+    "HH117-SI-MILF-nonINF-HLADR-AND-CD19" = list(
+      # new_cluster = c(old clusters)
+      "0" = c("0"),
+      "1" = c("1", "3", "5", "7"),
+      "2" = c("2"),
+      "3" = c("4"),
+      "6" = c("6"),
+      "5" = c("8")
+    ),
+    
+    "HH117-SI-PP-nonINF-HLADR-AND-CD19-AND-GC-AND-TFH" = list(
+      # new_cluster = c(old clusters)
+      "0" = c("0"),
+      "1" = c("1"),
+      "2" = c("2", "3"),
+      "3" = c("4"),
+      "4" = c("5", "6", "7")
+    ),
+    
+    "HH119-COLP-PC" = list(
+      # new_cluster = c(old clusters)
+      "0" = c("0"),
+      "1" = c("1"),
+      "2" = c("2")
+    ),
+    
+    "HH119-CO-SMILF-CD19-AND-GC-AND-PB-AND-TFH" = list(
+      # new_cluster = c(old clusters)
+      "0" = c("0", "2", "4"),
+      "1" = c("1"),
+      "2" = c("3"),
+      "3" = c("5")
+    ),
+    
+    "HH119-SILP-PC" = list(
+      # new_cluster = c(old clusters)
+      "0" = c("0", "1", "2"),
+      "1" = c("3"),
+      "2" = c("4"),
+      "3" = c("5"),
+      "4" = c("6"),
+      "5" = c("7")
+    ),
+    
+    "HH119-SI-MILF-CD19-AND-GC-AND-PB-AND-TFH" = list(
+      # new_cluster = c(old clusters)
+      "0" = c("0", "1", "6"),
+      "1" = c("2", "7"),
+      "2" = c("3", "4"),
+      "3" = c("5")
+    ),
+    
+    "HH119-SI-PP-CD19-Pool1" = list(
+      # new_cluster = c(old clusters)
+      "0" = c("0", "1", "5"),
+      "1" = c("2", "3", "4"),
+      "2" = c("6"),
+      "3" = c("7")
+    ),
+    
+    "HH119-SI-PP-CD19-Pool2" = list(
+      # new_cluster = c(old clusters)
+      "0" = c("0", "1", "4"),
+      "1" = c("2", "3"),
+      "2" = c("5"),
+      "3" = c("6")
+    ),
+    
+    "HH119-SI-PP-GC-AND-PB-AND-TFH-Pool1" = list(
+      # new_cluster = c(old clusters)
+      "0" = c("0", "2", "3", "5"),
+      "1" = c("1", "6"),
+      "2" = c("4"),
+      "3" = c("7")
+    ),
+    
+    "HH119-SI-PP-GC-AND-PB-AND-TFH-Pool2" = list(
+      # new_cluster = c(old clusters)
+      "0" = c("0", "2", "3"),
+      "1" = c("1", "6"),
+      "2" = c("4"),
+      "3" = c("5")
+    )
+  ),
+  
+  "v8" = list(
+    list(
+      
+      "HH117-SILP-INF-PC" = list(
+        # new_cluster = c(old clusters)
+        "0" = c("0", "2", "3", "4"),
+        "1" = c("1"),
+        "2" = c("5")
+      ),
+      
+      "HH117-SILP-nonINF-PC" = list(
+        # new_cluster = c(old clusters)
+        "0" = c("0", "2"),
+        "1" = c("1", "3")
+      ),
+      
+      "HH117-SI-MILF-INF-HLADR-AND-CD19" = list(
+        # new_cluster = c(old clusters)
+        "0" = c("0", "2", "4", "6"),
+        "1" = c("1"),
+        "2" = c("3"),
+        "3" = c("5")
+      ),
+      
+      "HH117-SI-MILF-nonINF-HLADR-AND-CD19" = list(
+        # new_cluster = c(old clusters)
+        "0" = c("0"),
+        "1" = c("1", "3", "5", "6"),
+        "2" = c("2"),
+        "3" = c("4"),
+        "4" = c("7"),
+        "5" = c("8")
+      ),
+      
+      "HH117-SI-PP-nonINF-HLADR-AND-CD19-AND-GC-AND-TFH" = list(
+        # new_cluster = c(old clusters)
+        "0" = c("0"),
+        "1" = c("1"),
+        "2" = c("2", "3"),
+        "3" = c("4"),
+        "4" = c("5", "6", "7")
+      ),
+      
+      "HH119-COLP-PC" = list(
+        # new_cluster = c(old clusters)
+        "0" = c("0"),
+        "1" = c("1"),
+        "2" = c("2")
+      ),
+      
+      "HH119-CO-SMILF-CD19-AND-GC-AND-PB-AND-TFH" = list(
+        # new_cluster = c(old clusters)
+        "0" = c("0", "2", "4"),
+        "1" = c("1"),
+        "2" = c("3"),
+        "3" = c("5")
+      ),
+      
+      "HH119-SILP-PC" = list(
+        # new_cluster = c(old clusters)
+        "0" = c("0", "1", "2"),
+        "1" = c("3"),
+        "2" = c("4"),
+        "3" = c("5"),
+        "4" = c("6"),
+        "5" = c("7")
+      ),
+      
+      "HH119-SI-MILF-CD19-AND-GC-AND-PB-AND-TFH" = list(
+        # new_cluster = c(old clusters)
+        "0" = c("0", "1", "6"),
+        "1" = c("2", "7"),
+        "2" = c("3", "5"),
+        "3" = c("4")
+      ),
+      
+      "HH119-SI-PP-CD19-Pool1" = list(
+        # new_cluster = c(old clusters)
+        "0" = c("0", "1", "5"),
+        "1" = c("2", "3", "4"),
+        "2" = c("6"),
+        "3" = c("7")
+      ),
+      
+      "HH119-SI-PP-CD19-Pool2" = list(
+        # new_cluster = c(old clusters)
+        "0" = c("0", "1", "3"),
+        "1" = c("2", "4"),
+        "2" = c("5"),
+        "3" = c("6")
+      ),
+      
+      "HH119-SI-PP-GC-AND-PB-AND-TFH-Pool1" = list(
+        # new_cluster = c(old clusters)
+        "0" = c("0", "2", "3", "5"),
+        "1" = c("1", "6"),
+        "2" = c("4"),
+        "3" = c("7")
+      ),
+      
+      "HH119-SI-PP-GC-AND-PB-AND-TFH-Pool2" = list(
+        # new_cluster = c(old clusters)
+        "0" = c("0", "2", "3"),
+        "1" = c("1", "6"),
+        "2" = c("4"),
+        "3" = c("5")
+      )
+      
+    )
+  )
+  
+  
+  
+)
