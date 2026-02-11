8ccfcd84a64d088512828fed539e9e9964ca0e8d
helenewegener1 <helene.wegener@gmaill.com>
2026-02-11 16:56:09 +0100

    update script

--- 05_run_cellranger/web_summaries/script.sh
+++ 05_run_cellranger/web_summaries/script.sh
@@ -1,13 +1,9 @@
 # More efficient: directly target the expected structure
-for sample_dir in ../out_v9/res_* ../out_v8/res_*; do
-    if [ -d "$sample_dir" ]; then
-        # Look for web_summary.html in the expected location
-        html_file="$sample_dir/outs/per_sample_outs/$(basename $sample_dir)/web_summary.html"
-        
-        if [ -f "$html_file" ]; then
-            sample=$(basename "$sample_dir")
-            cp "$html_file" "web_summaries/${sample}_web_summary.html"
-            echo "Copied: $sample"
-        fi
-    fi
+for sample_dir in ../out_v9/res_*; do
+   
+    # Look for web_summary.html in the expected location
+    html_file="$sample_dir/outs/per_sample_outs/$(basename $sample_dir)/web_summary.html"
+    sample=$(basename "$sample_dir")
+    cp "$html_file" "./${sample}_web_summary.html"
+    
 done
