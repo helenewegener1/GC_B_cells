b9e455a313f85bb8d51c8ea3b26d87327ffff85b
helenewegener1 <helene.wegener@gmail.com>
2026-02-12 13:32:56 +0100

    Stop tracking large HTML reports and plot outputs

--- 05_run_cellranger/web_summaries/script.sh
+++
@@ -1,9 +0,0 @@
-# More efficient: directly target the expected structure
-for sample_dir in ../out_v9/res_*; do
-   
-    # Look for web_summary.html in the expected location
-    html_file="$sample_dir/outs/per_sample_outs/$(basename $sample_dir)/web_summary.html"
-    sample=$(basename "$sample_dir")
-    cp "$html_file" "./${sample}_web_summary.html"
-    
-done
