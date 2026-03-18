05087a108af527212bd382b7f2631d171a6220b6
helenewegener1 <helene.wegener@gmail.com>
2026-03-18 13:17:54 +0100

    running change o script

--- 45_changeo/script/02_run_changeo.sh
+++ 45_changeo/script/02_run_changeo.sh
@@ -8,7 +8,7 @@ FILTERED_CONTIGS_PATH="${CELLRANGER_OUT_PATH}/filtered_contig.fasta"
 FILTERED_CONTIGS_ANNOTATIONS_PATH="${CELLRANGER_OUT_PATH}/filtered_contig_annotations.csv"
 
 # Output directory
-OUTDIR="/home/projects/dtu_00062/people/helweg/projects/GC_B_cells/06_changeo/${SAMPLE}"
+OUTDIR="/home/projects/dtu_00062/people/helweg/projects/GC_B_cells/45_changeo/${SAMPLE_NAME}"
 mkdir -p $OUTDIR
 
 # Step 1: V(D)J assignment with IgBLAST
@@ -16,35 +16,37 @@ singularity exec $IMAGE AssignGenes.py igblast \
     -s $FILTERED_CONTIGS_PATH \
     -b /usr/local/share/igblast \
     --organism human --loci ig --format blast \
-    --outdir $OUTDIR --outname $SAMPLE --nproc 4
+    --outdir $OUTDIR --outname $SAMPLE_NAME --nproc 4
 
 # Step 2: Parse into AIRR format, merging Cell Ranger barcode annotations
 singularity exec $IMAGE MakeDb.py igblast \
-    -i ${OUTDIR}/${SAMPLE}_igblast.fmt7 \
+    -i ${OUTDIR}/${SAMPLE_NAME}_igblast.fmt7 \
     -s $FILTERED_CONTIGS_PATH \
     -r /usr/local/share/germlines/imgt/human/vdj/ \
     --10x $FILTERED_CONTIGS_ANNOTATIONS_PATH \
-    --extended --outdir $OUTDIR --outname $SAMPLE
+    --extended --outdir $OUTDIR --outname $SAMPLE_NAME
 
 # Step 3: Filter to productive sequences
 singularity exec $IMAGE ParseDb.py select \
-    -d ${OUTDIR}/${SAMPLE}_db-pass.tsv \
+    -d ${OUTDIR}/${SAMPLE_NAME}_db-pass.tsv \
     -f productive -u T \
-    --outdir $OUTDIR --outname $SAMPLE
+    --outdir $OUTDIR --outname $SAMPLE_NAME
 
 # Step 4: Split heavy and light chains
 singularity exec $IMAGE ParseDb.py select \
-    -d ${OUTDIR}/${SAMPLE}_select-pass.tsv \
+    -d ${OUTDIR}/${SAMPLE_NAME}_select-pass.tsv \
     -f locus -u IGH --regex \
-    --outname ${SAMPLE}_heavy --outdir $OUTDIR
+    --outname ${SAMPLE_NAME}_heavy --outdir $OUTDIR
 
 singularity exec $IMAGE ParseDb.py select \
-    -d ${OUTDIR}/${SAMPLE}_select-pass.tsv \
+    -d ${OUTDIR}/${SAMPLE_NAME}_select-pass.tsv \
     -f locus -u "IG[LK]" --regex \
-    --outname ${SAMPLE}_light --outdir $OUTDIR
+    --outname ${SAMPLE_NAME}_light --outdir $OUTDIR
 
 # Step 5: Reconstruct germlines
 singularity exec $IMAGE CreateGermlines.py \
-    -d ${OUTDIR}/${SAMPLE}_heavy_select-pass.tsv \
+    -d ${OUTDIR}/${SAMPLE_NAME}_heavy_select-pass.tsv \
     -r /usr/local/share/germlines/imgt/human/vdj/ \
-    --cloned --outdir $OUTDIR --outname ${SAMPLE}_heavy
+    --cloned --outdir $OUTDIR --outname ${SAMPLE_NAME}_heavy
+    
+  
