3ad8bb8f13c19216f26d3b845eaaa4af7c251fdb
helenewegener1 <helene.wegener@gmaill.com>
2026-03-24 13:29:02 +0100

    dowser trees update

--- 45_immcantation/script/11_dowser.R
+++ 45_immcantation/script/11_dowser.R
@@ -33,7 +33,7 @@ HH_spec_clones_vj$germline_alignment_d_mask %>% head()
 # fetch_imgtdb.sh -o germlines # Run script to obtain IMGT gapped sequences
 
 # Read in IMGT-gapped sequences
-references <- readIMGT(dir = "../packages/immcantation/scripts/germlines/human/vdj")
+# references <- readIMGT(dir = "../packages/immcantation/scripts/germlines/human/vdj")
 
 # remove germline alignment columns for this example
 db <- select(HH_spec_clones_vj, -"germline_alignment", -"germline_alignment_d_mask")
@@ -55,15 +55,20 @@ HH_spec_clones_vj$germline_alignment_d_mask[1]
 # ------------------------------------------------------------------------------
 
 # Top clone
-clone_nr <- 1
+clone_nr <- 5
 clone <- HH_spec_clones_vj %>% count(clone_id, sort = TRUE) %>% head(1) %>% pull(clone_id)
 
 # Subset data for this example
-HH_spec_clones_vj = HH_spec_clones_vj[HH_spec_clones_vj$clone_id == clone,]
-# HH_spec_clones_vj$subject_id = "HH119"
+HH_spec_clones_vj <- HH_spec_clones_vj[HH_spec_clones_vj$clone_id == clone,]
+
+# Add count for identical clones - used for tipsize of tree
+HH_spec_clones_vj <- HH_spec_clones_vj %>%
+  group_by(clone_id, sequence_alignment) %>%
+  mutate(n_identical = n()) %>%
+  ungroup()
 
 # Process example data using default settings
-clones <- formatClones(HH_spec_clones_vj, text_fields = "c_call")
+clones <- formatClones(HH_spec_clones_vj, text_fields = c("c_call", "celltype_broad"), num_fields=c("n_identical"))
 
 print(clones)
 
@@ -86,10 +91,26 @@ clones <- getTrees(clones, build="pml")
 plotTrees(
   clones, 
   tips="c_call", 
+  tipsize="n_identical",
   title = FALSE
-)[[1]] + plot_annotation(title = glue("{HH}: Clone number {clone_nr}"))
+)[[1]] + 
+  plot_annotation(
+    title = glue("{HH}: Clone number {clone_nr}")
+  )
 
-ggsave(glue("45_immcantation/plot/11_drowser/{HH}_drowser_tree_clone_{clone_nr}.png"), width = 15, height = 15)
+ggsave(glue("45_immcantation/plot/11_dowser/{HH}_dowser_tree_clone_{clone_nr}_c_call.png"), width = 15, height = 25)
+
+plotTrees(
+  clones, 
+  tips="celltype_broad", 
+  tipsize="n_identical",
+  title = FALSE
+)[[1]] + 
+  plot_annotation(
+    title = glue("{HH}: Clone number {clone_nr}")
+  )
+
+ggsave(glue("45_immcantation/plot/11_dowser/{HH}_dowser_tree_clone_{clone_nr}_celltype.png"), width = 15, height = 25)
 
 # ------------------------------------------------------------------------------
 # 
