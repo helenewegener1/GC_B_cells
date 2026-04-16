3b08c6e0504e9ed63d56e630c394786fdb6e5a0a
helenewegener1 <helene.wegener@gmaill.com>
2026-04-16 19:09:59 +0200

    update

--- 46_sequence_driven_clustering/script/01_hierical_clustering.R
+++ 46_sequence_driven_clustering/script/01_hierical_clustering.R
@@ -10,47 +10,49 @@ library(fastcluster)
 # Load data
 # ------------------------------------------------------------------------------
 
-spec_clones_vj <- readRDS("45_immcantation/out/rds/spec_clones_vj.rds")
+spec_clones_vj <- readRDS("45_immcantation/out/rds/05_spec_clones_vj_heavy.rds")
 
 # ------------------------------------------------------------------------------
 # Extract sequences
 # ------------------------------------------------------------------------------
 
-HH <- "HH117"
+HH <- "HH119"
 
 # Define patient
-spec_clones_vj_HH <- spec_clones_vj[[HH]] %>% 
-  mutate(
-    v_gene = v_call %>% str_split_i("\\*", 1) %>% unique() %>% paste0(collapse = ", "), .by = v_call
-  ) %>% 
-  mutate(
-    j_gene = j_call %>% str_split_i("\\*", 1) %>% unique() %>% paste0(collapse = ", "), .by = j_call
-  )
+spec_clones_vj_HH <- spec_clones_vj[[HH]]
 
 # Look at top heavy chain clones defined with SCOPer
 spec_clones_vj_HH %>% 
-  count(clone_id, v_gene, j_gene, sort = TRUE)
+  count(clone_id, v_call_majority, j_call_majority, sort = TRUE)
 
 # Get top 10 clones
-top_clones <- spec_clones_vj_HH %>% count(clone_id, sort = TRUE) %>% head(n = 10) %>% pull(clone_id)
+top_clones <- spec_clones_vj_HH %>% count(clone_id, sort = TRUE) %>% head(n = 5) %>% pull(clone_id)
 
 # Check N cells in top 10 clones
 spec_clones_vj_HH %>% filter(clone_id %in% top_clones) %>% nrow()
 
 # Get genes for the clones 
-v_genes <- spec_clones_vj_HH %>% filter(clone_id %in% top_clones) %>% pull(v_gene) %>% unique()
-j_genes <- spec_clones_vj_HH %>% filter(clone_id %in% top_clones) %>% pull(j_gene) %>% unique()
+v_genes <- spec_clones_vj_HH %>% filter(clone_id %in% top_clones) %>% pull(v_call_majority) %>% unique() %>% str_split(",") %>% unlist() %>% unique()
+j_genes <- spec_clones_vj_HH %>% filter(clone_id %in% top_clones) %>% pull(j_call_majority) %>% unique() %>% str_split(",") %>% unlist() %>% unique()
+
+# Build a regex pattern with escaped asterisks
+v_gene_regex <- paste(str_replace_all(v_genes, "\\*", "\\\\*"), collapse = "|")
+j_gene_regex <- paste(str_replace_all(j_genes, "\\*", "\\\\*"), collapse = "|")
+
+spec_clones_vj_HH %>% 
+  filter(str_detect(v_call_majority, v_gene_regex)) %>% 
+  count(v_call_majority, sort = TRUE)
 
 # Get all sequences
-seqs <- spec_clones_vj_HH %>% filter(v_gene %in% v_genes, j_gene %in% j_genes) %>% pull(sequence)
-seq_names <- spec_clones_vj_HH %>% filter(v_gene %in% v_genes, j_gene %in% j_genes) %>% pull(sequence_id)
+seqs <- spec_clones_vj_HH %>% filter(str_detect(v_call_majority, v_gene_regex), str_detect(j_call_majority, j_gene_regex)) %>% pull(sequence)
+seq_names <- spec_clones_vj_HH %>% filter(str_detect(v_call_majority, v_gene_regex), str_detect(j_call_majority, j_gene_regex)) %>% pull(sequence_id)
 
 seqs %>% length()
 seqs %>% unique() %>% length()
 
 # Get metadata of sequences
 seqs_meta <- spec_clones_vj_HH %>% 
-  filter(v_gene %in% v_genes, j_gene %in% j_genes) %>% 
+  filter(str_detect(v_call_majority, v_gene_regex), str_detect(j_call_majority, j_gene_regex)) %>% 
   mutate(
     label = sequence_id, 
     junction_length = as.character(junction_length)
@@ -98,7 +100,10 @@ n_clones <- seqs_meta$clone_id %>% unique() %>% length()
 ## Wrangle metadata to only color the top 10 clones
 seqs_meta <- seqs_meta %>% 
   mutate(
-    clone_id_top_10 = ifelse(clone_id %in% top_clones, clone_id, "other")
+    clone_id_top_10 = ifelse(clone_id %in% top_clones, clone_id, "other"), 
+    v_gene = v_call_majority %>% str_split_i(",", 1) %>% str_split_i("\\*", 1),
+    j_gene = j_call_majority %>% str_split_i(",", 1) %>% str_split_i("\\*", 1), 
+    v_j_junction = paste(v_gene, j_gene, junction_length, sep = "_")
   )
 
 seqs_meta$clone_id_top_10 %>% table(useNA = "always") 
@@ -109,17 +114,17 @@ clone_colors <- setNames(
     "#4CAF50",  
     "#FF9800",  
     "#9C27B0",  
-    "#00BCD4",  
-    "#FFEB3B",  
-    "#FF4081",  
-    "#795548",  
-    "#76FF03", 
+    # "#00BCD4",  
+    # "#FFEB3B",  
+    # "#FF4081",  
+    # "#795548",  
+    # "#76FF03", 
     "grey90"), 
   c(top_clones, "other")
 ) # NAs will be grey
 
 ggtree(tree_phylo, layout="fan", size=0.2) %<+% seqs_meta + 
-  geom_tippoint(aes(color = clone_id_top_10), size=0.5, alpha=0.8) +
+  geom_tippoint(aes(color = clone_id_top_10), size=0.1, alpha=0.8) +
   theme_tree2() + 
   scale_color_manual(values = clone_colors) + 
   guides(color = guide_legend(override.aes = list(size = 4))) +
@@ -129,7 +134,7 @@ ggtree(tree_phylo, layout="fan", size=0.2) %<+% seqs_meta +
 ggsave(glue("46_sequence_driven_clustering/plot/{HH}_clone_ID_fan.png"), width = 18, height = 16, dpi = 1000)
 
 ggtree(tree_phylo, layout="rectangular", size=0.2) %<+% seqs_meta + 
-  geom_tippoint(aes(color = clone_id_top_10), size=0.5, alpha=0.8) +
+  geom_tippoint(aes(color = clone_id_top_10), size=0.1, alpha=0.8) +
   theme_tree2() + 
   scale_color_manual(values = clone_colors) + 
   guides(color = guide_legend(override.aes = list(size = 4))) +
@@ -147,18 +152,43 @@ ggtree(tree_phylo, layout="fan", size=0.2) %<+% seqs_meta +
   labs(title = glue("{HH} colored by V gene")) 
 ggsave(glue("46_sequence_driven_clustering/plot/{HH}_v_gene.png"), width = 18, height = 16, dpi = 1000)
 
+# Color by V call majority
 ggtree(tree_phylo, layout="fan", size=0.2) %<+% seqs_meta + 
-  geom_tippoint(aes(color = j_gene), size=0.5, alpha=0.8) +
+  geom_tippoint(aes(color = v_call_majority), size=0.5, alpha=0.8) +
   theme_tree2() + 
-  labs(title = glue("{HH} colored by J gene"))
-ggsave(glue("46_sequence_driven_clustering/plot/{HH}_j_gene.png"), width = 18, height = 16, dpi = 1000)
+  guides(color = guide_legend(override.aes = list(size = 4))) + 
+  labs(title = glue("{HH} colored by V gene")) 
+ggsave(glue("46_sequence_driven_clustering/plot/{HH}_v_call_majority.png"), width = 18, height = 16, dpi = 1000)
 
-ggtree(tree_phylo, layout="fan", size=0.5) %<+% seqs_meta + 
+# Color by J gene
+ggtree(tree_phylo, layout="fan", size=0.2) %<+% seqs_meta + 
+  geom_tippoint(aes(color = j_gene), size=0.2, alpha=0.8) +
+  theme_tree2() + 
+  guides(color = guide_legend(override.aes = list(size = 4))) + 
+  labs(title = glue("{HH} colored by J gene")) 
+ggsave(glue("46_sequence_driven_clustering/plot/{HH}_J_gene.png"), width = 18, height = 16, dpi = 1000)
+
+# Color by J call majority
+ggtree(tree_phylo, layout="fan", size=0.2) %<+% seqs_meta + 
+  geom_tippoint(aes(color = j_call_majority), size=0.5, alpha=0.8) +
+  theme_tree2() + 
+  guides(color = guide_legend(override.aes = list(size = 4))) + 
+  labs(title = glue("{HH} colored by J gene")) 
+ggsave(glue("46_sequence_driven_clustering/plot/{HH}_j_call_majority.png"), width = 18, height = 16, dpi = 1000)
+
+# Color by junction length
+ggtree(tree_phylo, layout="fan", size=0.2) %<+% seqs_meta + 
   geom_tippoint(aes(color = junction_length), size=0.5, alpha=0.8) +
   theme_tree2() + 
   labs(title = glue("{HH} colored by junction length"))
 ggsave(glue("46_sequence_driven_clustering/plot/{HH}_junction_length.png"), width = 18, height = 16, dpi = 1000)
 
+# Color by v_j_junction
+ggtree(tree_phylo, layout="fan", size=0.2) %<+% seqs_meta + 
+  geom_tippoint(aes(color = v_j_junction), size=0.5, alpha=0.8) +
+  theme_tree2() + 
+  labs(title = glue("{HH} colored by v_j_junction"))
+ggsave(glue("46_sequence_driven_clustering/plot/{HH}_v_j_junction.png"), width = 22, height = 16, dpi = 1000)
 
 
 
