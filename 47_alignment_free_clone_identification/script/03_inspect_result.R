09c879cda43d5307fdd2bd150fd7312f45bb94e9
helenewegener1 <helene.wegener@gmaill.com>
2026-04-26 13:37:02 +0200

    compare alignment free to SCOPer

--- 47_alignment_free_clone_identification/script/03_inspect_result.R
+++ 47_alignment_free_clone_identification/script/03_inspect_result.R
@@ -7,7 +7,7 @@ library(glue)
 
 LC_list <- readRDS("45_immcantation/out/rds/resolve_LC_list.rds")
 
-HH <- "HH119"
+HH <- "HH117"
 align_free <- read.delim(glue("47_alignment_free_clone_identification/out/{HH}_clone_output.tsv"))
 
 LC <- LC_list[[HH]]
@@ -30,11 +30,11 @@ align_free %>% filter(ID %in% LC_clone_cell_id) %>%
   count(CLONE, sort = TRUE) %>% head(n = 10)
 
 # ------------------------------------------------------------------------------
-# Visualize 
+# Visualize - LC clones (heavy)
 # ------------------------------------------------------------------------------
 
 outdir <- glue("47_alignment_free_clone_identification/plot/LC_vs_align_free")
-dir.create(outdir, showWarnings = FALSE)
+dir.create(outdir, recursive = TRUE, showWarnings = FALSE)
 
 # Clean dfs
 LC_clean <- LC %>% filter(locus == "IGH") %>% select(cell_id, clone_id)
@@ -54,16 +54,15 @@ top_clones_count <- list(
 names(top_clones_names$LC) <- paste("Clone", 1:10, "\nN cells: ", top_clones_count$LC)
 names(top_clones_names$align_free) <- paste("Clone", 1:10, "\nN cells: ", top_clones_count$align_free)
 
-
 # Make table
 compare <- table(LC_clean$clone_id, align_free_clean$CLONE) %>% 
   as.data.frame() %>%
   setNames(c("LC_clones", "align_free_clones", "Count")) 
  
 # LC clones in align_free 
-min_count <- 4
+min_count <- 2
 compare %>% 
-  filter(LC_clones %in% top_clones_names$LC & Count > min_count) %>% 
+  filter(LC_clones %in% top_clones_names$LC & Count >= min_count) %>% 
   ggplot(aes(x = LC_clones, y = align_free_clones, fill = Count)) +
   geom_tile(color = "white", linewidth = 0.4) +
   geom_text(aes(label = Count), size = 3, color = "black") +
@@ -75,7 +74,7 @@ compare %>%
   labs(
     x     = "LC clones",
     y     = "Alignment free clones",
-    title = glue("{HH}: Top 10 LC clusters VS Alignment free clones"),
+    title = glue("{HH}: Top 10 LC VS Alignment free clones"),
     caption = glue("Minimum count: {min_count}")
   ) +
   theme_minimal(base_size = 10) + 
@@ -88,9 +87,9 @@ compare %>%
 ggsave(glue("{outdir}/{HH}_LC_VS_align_free_clones.png"), width = 10, height = 9)
 
 # align_free clones in LC
-min_count <- 4
+min_count <- 2
 compare %>% 
-  filter(align_free_clones %in% top_clones_names$align_free & Count > min_count) %>% 
+  filter(align_free_clones %in% top_clones_names$align_free & Count >= min_count) %>% 
   ggplot(aes(x = align_free_clones, y = LC_clones, fill = Count)) +
   geom_tile(color = "white", linewidth = 0.4) +
   geom_text(aes(label = Count), size = 3, color = "black") +
@@ -102,7 +101,7 @@ compare %>%
   labs(
     x     = "Alignment free clones",
     y     = "LC clones",
-    title = glue("{HH}: Top 10 Alignment free VS LC clusters clones"),
+    title = glue("{HH}: Top 10 Alignment free VS LC clones"),
     caption = glue("Minimum count: {min_count}")
   ) +
   theme_minimal(base_size = 10) + 
@@ -113,6 +112,91 @@ compare %>%
   )
 
 ggsave(glue("{outdir}/{HH}_align_free_VS_LC_clones.png"), width = 10, height = 11)
+
+
+# ------------------------------------------------------------------------------
+# Visualize - LC subclones (heavy and light)
+# ------------------------------------------------------------------------------
+
+outdir <- glue("47_alignment_free_clone_identification/plot/LC_subclone_vs_align_free")
+dir.create(outdir, recursive = TRUE, showWarnings = FALSE)
+
+# Clean dfs
+LC_clean <- LC %>% filter(locus == "IGH") %>% select(cell_id, clone_subgroup_id)
+align_free_clean <- align_free %>%  select(ID, CLONE)
+
+# Define top clones
+top_clones_names <- list(
+  "LC" = LC_clean %>% count(clone_subgroup_id, sort = TRUE) %>% head(10) %>% pull(clone_subgroup_id),
+  "align_free" = align_free_clean %>% count(CLONE, sort = TRUE) %>% head(10) %>% pull(CLONE) %>% as.character()
+)
+
+top_clones_count <- list(
+  "LC" = LC_clean %>% count(clone_subgroup_id, sort = TRUE) %>% head(10) %>% pull(n),
+  "align_free" = align_free_clean %>% count(CLONE, sort = TRUE) %>% head(10) %>% pull(n) 
+)
+
+names(top_clones_names$LC) <- paste("Clone", 1:10, "\nN cells: ", top_clones_count$LC)
+names(top_clones_names$align_free) <- paste("Clone", 1:10, "\nN cells: ", top_clones_count$align_free)
+
+# Make table
+compare <- table(LC_clean$clone_subgroup_id, align_free_clean$CLONE) %>% 
+  as.data.frame() %>%
+  setNames(c("LC_clones", "align_free_clones", "Count")) 
+
+# LC clones in align_free 
+min_count <- 2
+compare %>% 
+  filter(LC_clones %in% top_clones_names$LC & Count >= min_count) %>% 
+  ggplot(aes(x = LC_clones, y = align_free_clones, fill = Count)) +
+  geom_tile(color = "white", linewidth = 0.4) +
+  geom_text(aes(label = Count), size = 3, color = "black") +
+  scale_fill_gradient(low = "#EEF3FB", high = "#185FA5") +
+  scale_x_discrete(
+    limits = top_clones_names$LC,
+    labels = setNames(names(top_clones_names$LC), top_clones_names$LC)
+  ) + 
+  labs(
+    x     = "LC clones",
+    y     = "Alignment free clones",
+    title = glue("{HH}: Top 10 LC (sub) VS Alignment free clones"),
+    caption = glue("Minimum count: {min_count}")
+  ) +
+  theme_minimal(base_size = 10) + 
+  theme(
+    # axis.text.x  = element_text(angle = 45, hjust = 1),
+    panel.grid   = element_blank(),
+    legend.position = "none"
+  )
+
+ggsave(glue("{outdir}/{HH}_LCsub_VS_align_free_clones.png"), width = 10, height = 12)
+
+# align_free clones in LC
+min_count <- 2
+compare %>% 
+  filter(align_free_clones %in% top_clones_names$align_free & Count >= min_count) %>% 
+  ggplot(aes(x = align_free_clones, y = LC_clones, fill = Count)) +
+  geom_tile(color = "white", linewidth = 0.4) +
+  geom_text(aes(label = Count), size = 3, color = "black") +
+  scale_fill_gradient(low = "#EEF3FB", high = "#185FA5") +
+  scale_x_discrete(
+    limits = top_clones_names$align_free,
+    labels = setNames(names(top_clones_names$align_free), top_clones_names$align_free)
+  ) + 
+  labs(
+    x     = "Alignment free clones",
+    y     = "LC clones",
+    title = glue("{HH}: Top 10 Alignment free VS LC (sub) clones"),
+    caption = glue("Minimum count: {min_count}")
+  ) +
+  theme_minimal(base_size = 10) + 
+  theme(
+    # axis.text.x  = element_text(angle = 45, hjust = 1),
+    panel.grid   = element_blank(),
+    legend.position = "none"
+  )
+
+ggsave(glue("{outdir}/{HH}_align_free_VS_LCsub_clones.png"), width = 10, height = 12)
 
 
 
