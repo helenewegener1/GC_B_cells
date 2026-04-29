5dcd4b17264af051ab1e339e42695af5462f78d6
helenewegener1 <helene.wegener@gmaill.com>
2026-04-29 10:52:19 +0200

    more GCtrees

--- 48_GCtree/script/02_run_gctree.sh
+++ 48_GCtree/script/02_run_gctree.sh
@@ -12,7 +12,8 @@ sample_list=$(ls $DATA_DIR | cut -d "." -f1)
 # # Run gctree
 # for sample in $sample_list; do
 
-  sample=HH117_clone_nr_10_clone_2587_1
+  # sample=HH117_clone_nr_10_clone_2587_1
+  sample=HH117_clone_nr_6_clone_1278_1
 
   # Make sample sepecific outdir
   OUT_DIR_SAMPLE=${OUT_DIR}/$sample
@@ -20,7 +21,7 @@ sample_list=$(ls $DATA_DIR | cut -d "." -f1)
   cd $OUT_DIR_SAMPLE
   
   # Clean up before new run 
-  rm outfile outtree
+  rm -f outfile outtree
 
   # Deduplication and sequence abundances
   deduplicate $DATA_DIR/${sample}.fasta \
@@ -31,22 +32,13 @@ sample_list=$(ls $DATA_DIR | cut -d "." -f1)
   # Parsimony trees
   mkconfig deduplicated.phylip dnapars > dnapars.cfg
   dnapars < dnapars.cfg > dnapars.log
+  
+  # Make plotting sepecific outdir
+  PLOT_DIR_SAMPLE=${PLOT_DIR}/$sample
+  mkdir -p $PLOT_DIR_SAMPLE
 
   # Gctree Ranking
-  # gctree infer outfile abundances.csv --root GL --frame 1 --verbose
-  xvfb-run -a gctree infer outfile abundances.csv --root GL --frame 1 --verbose --outbase $PLOT_DIR/$sample
+  xvfb-run -a gctree infer outfile abundances.csv --root GL --frame 1 --verbose --outbase $PLOT_DIR_SAMPLE/$sample
 
 # done
 
-# 
-# # Extra?
-# export QT_QPA_PLATFORM=offscreen
-# export XDG_RUNTIME_DIR=/tmp/runtime-runner
-# export MPLBACKEND=agg
-# export MPLCONFIGDIR=/home/projects/dtu_00062/people/helweg/projects/GC_B_cells/.matplotlib
-# 
-# gctree infer outfile abundances.csv --root GL --frame 1 --verbose
-
-# export CONDA_PKGS_DIRS=/home/projects/dtu_00062/people/helweg/projects/GC_B_cells/.conda_pkgs
-# 
-# /services/tools/anaconda3/2024.06-1/bin/conda create --prefix /home/projects/dtu_00062/people/helweg/projects/GC_B_cells/48_GCtree/script/gctree_env_310 python=3.10
