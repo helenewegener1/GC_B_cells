cdf0e8ffe18bc75eedd6dde1178642ebcb1f4129
helenewegener1 <helene.wegener@gmaill.com>
2026-05-21 14:15:20 +0200

    slingshot plots for more clones

--- 49_trajectory_analysis/script/01_prep.R
+++ 49_trajectory_analysis/script/01_prep.R
@@ -42,99 +42,191 @@ DimPlot(seurat_integrated, group.by = "L1_annotation", split.by = "patient", red
 # Investigate largest CRC clone
 # ------------------------------------------------------------------------------
 
-HH <- "HH117"
-clone <- "1849_1"
+# HH <- "HH117"
+# clone <- "1849_1" # 3rd clone
 
-# clone_nr <- 3
-# # Get clone
-# clone <- seurat_integrated[[]] %>% 
-#   filter(
-#     patient == HH, 
-#     !is.na(clone_subgroup_id)
-#   ) %>% 
-#   dplyr::count(clone_subgroup_id, sort = TRUE) %>% 
-#   dplyr::slice(clone_nr) %>% 
-#   pull(clone_subgroup_id)
+# HH <- "HH119"
+# clone <- "28075_1" # 3rd clone
 
-# Subset seurat object 
-sub <- subset(seurat_integrated, patient == HH & clone_subgroup_id == clone)
-ncol(sub)
-
-# Re-normalize 
-sub <- NormalizeData(sub)
-sub <- FindVariableFeatures(sub)
-sub <- ScaleData(sub)
-sub <- RunPCA(sub)
-
-# Re-integrate 
-# No need to integrate since we are working with 1 patient 
-
-Reductions(sub)
-
-# Re-cluster on new embedding
-sub <- FindNeighbors(sub, reduction = "pca")
-sub <- FindClusters(sub, res = 0.2)
-sub <- RunUMAP(sub, reduction = "pca", dims = 1:30)
-
-# Plot
-DimPlot(sub, group.by = "sample_clean_plot", reduction = "umap")
-
-DimPlot(sub, group.by = "L1_annotation", reduction = "umap") + 
-  scale_color_manual(values = L1_colors)
-
-DimPlot(sub, group.by = "c_call", reduction = "umap") + 
-  scale_color_manual(values = isotype_colors_custom)
-
-DimPlot(sub, group.by = "seurat_clusters", reduction = "umap")
-
-# ------------------------------------------------------------------------------
-# slingshot 
-# ------------------------------------------------------------------------------
-
-# Transform Seurat object to single cell experiment 
-sce <- SingleCellExperiment(
-  assays = list(
-    counts = GetAssayData(sub, layer = "counts"),
-    logcounts = GetAssayData(sub, layer = "data")
+all_clones <- list(
+  "HH117" = c(
+    "4221_1", "2628_1", "1849_1", "3709_1", "2301_1",
+    "1320_1", "5941_1", "6115_1", "1910_1", "2169_1"
   ),
-  colData = sub@meta.data,
-  reducedDims = list(
-    PCA = Embeddings(sub, "pca"),
-    UMAP = Embeddings(sub, "umap")
+  
+  "HH119" = c(
+    "28075_1", "12120_1", "15287_1", "23124_1",
+    "8372_1", "3869_1", "25158_1", "7913_1"
   )
 )
 
-# N cells 
-ncol(sce)
+for (HH in names(all_clones)){
+  
+  HH_clones <- all_clones[[HH]]
+  
+  for (clone in HH_clones){
+    
+    # Subset seurat object 
+    sub <- subset(seurat_integrated, patient == HH & clone_subgroup_id == clone)
+    ncol(sub)
+    
+    # Re-normalize 
+    sub <- NormalizeData(sub)
+    sub <- FindVariableFeatures(sub)
+    sub <- ScaleData(sub)
+    sub <- RunPCA(sub, npcs = 30)
+    
+    # Re-integrate 
+    # No need to integrate since we are working with 1 patient 
+    
+    Reductions(sub)
+    
+    # Re-cluster on new embedding
+    sub <- FindNeighbors(sub, reduction = "pca")
+    sub <- FindClusters(sub, res = 0.5)
+    sub <- RunUMAP(sub, reduction = "pca", dims = 1:30)
+    
+    # Plot
+    DimPlot(sub, group.by = "sample_clean_plot", reduction = "umap")
+    
+    DimPlot(sub, group.by = "L1_annotation", reduction = "umap") + 
+      scale_color_manual(values = L1_colors)
+    
+    DimPlot(sub, group.by = "c_call", reduction = "umap") + 
+      scale_color_manual(values = isotype_colors_custom)
+    
+    DimPlot(sub, group.by = "seurat_clusters", reduction = "umap")
+    
+    # ------------------------------------------------------------------------------
+    # slingshot - prep
+    # ------------------------------------------------------------------------------
+    
+    # Transform Seurat object to single cell experiment 
+    sce <- SingleCellExperiment(
+      assays = list(
+        counts = GetAssayData(sub, layer = "counts"),
+        logcounts = GetAssayData(sub, layer = "data")
+      ),
+      colData = sub@meta.data,
+      reducedDims = list(
+        PCA = Embeddings(sub, "pca"),
+        UMAP = Embeddings(sub, "umap")
+      )
+    )
+    
+    # N cells 
+    ncol(sce)
+    
+    # N unique clones 
+    sce$junction %>% unique() %>% length()
+    
+    # Define clusterLabel 
+    counts <- table(sce$junction)
+    clusterLabel <- ifelse(counts[sce$junction] == 1, -1, sce$junction)
+    table(clusterLabel)
+    
+    # prep outdir 
+    outdir <- glue("49_trajectory_analysis/plot/{HH}_{clone}/")
+    dir.create(outdir, recursive = TRUE, showWarnings = FALSE)
+    
+    # ------------------------------------------------------------------------------
+    # slingshot 
+    # ------------------------------------------------------------------------------
+    
+    # Run slingshot
+    # sce <- slingshot(sce, clusterLabels = clusterLabel, reducedDim = 'PCA')
+    sce <- slingshot(sce, clusterLabels = 'seurat_clusters', reducedDim = 'PCA')
+    
+    # Plot
+    summary(sce$slingPseudotime_1)
+    
+    # Trajectory
+    png(glue("{outdir}/{HH}_{clone}_slingshot.png"), res = 1000, width = 9000, height = 6000)
+    
+    colors <- colorRampPalette(brewer.pal(11,'Spectral')[-6])(100)
+    plotcol <- colors[cut(sce$slingPseudotime_1, breaks=100)]
+    plot(reducedDims(sce)$PCA, col = plotcol, pch=16, asp = 1, main = glue("{HH}: clone {clone}"))
+    lines(SlingshotDataSet(sce), lwd=2, col='black')
+    
+    dev.off()
+    
+    # # Seurat clusters 
+    # colors <- c("red", "blue")
+    # plotcol <- colors[sce$seurat_clusters]
+    # plot(reducedDims(sce)$PCA, col = plotcol, pch=16, asp = 1, main = glue("{HH}: clone {clone}"))
+    # lines(SlingshotDataSet(sce), lwd=2, col='black')
+    
+    # Isotype
+    png(glue("{outdir}/{HH}_{clone}_isotype.png"), res = 1000, width = 9000, height = 6000)
+    
+    colors <- isotype_colors_custom
+    plotcol <- colors[sce$c_call]
+    plot(reducedDims(sce)$PCA, col = plotcol, pch=16, asp = 1, main = glue("{HH}: clone {clone}"))
+    lines(SlingshotDataSet(sce), lwd=2, col='black')
+    
+    present <- unique(sce$c_call)
+    legend("topright",
+           legend = names(colors[present]),
+           col = colors[present],
+           pch = 16,
+           bty = "n",
+           cex = 0.8)
+    
+    dev.off()
+    
+    # Cell type
+    
+    png(glue("{outdir}/{HH}_{clone}_L1.png"), res = 1000, width = 9000, height = 6000)
+    
+    colors <- L1_colors
+    plotcol <- colors[sce$L1_annotation]
+    plot(reducedDims(sce)$PCA, col = plotcol, pch=16, asp = 1, main = glue("{HH}: clone {clone}"))
+    lines(SlingshotDataSet(sce), lwd=2, col='black')
+    
+    present <- unique(sce$L1_annotation)
+    legend("topright",
+           legend = names(colors[present]),
+           col = colors[present],
+           pch = 16,
+           bty = "n",
+           cex = 0.8)
+    
+    dev.off()
+    
+    # sample
+    png(glue("{outdir}/{HH}_{clone}_sample.png"), res = 1000, width = 9000, height = 6000)
+    
+    colors <- sample_clean_plot_colors
+    plotcol <- colors[sce$sample_clean_plot]
+    plot(reducedDims(sce)$PCA, col = plotcol, pch=16, asp = 1, main = glue("{HH}: clone {clone}"))
+    lines(SlingshotDataSet(sce), lwd=2, col='black')
+    
+    present <- unique(sce$sample_clean_plot)
+    legend("topright",
+           legend = names(colors[present]),
+           col = colors[present],
+           pch = 16,
+           bty = "n",
+           cex = 0.8)
+    
+    dev.off()
+    
+  }
+  
+}
 
-# N unique clones 
-sce$junction %>% unique() %>% length()
+# ------------------------------------------------------------------------------
+# slingshot::getLineages
+# ------------------------------------------------------------------------------
 
-# Run slingshot
-# sce <- slingshot(sce, clusterLabels = 'junction', reducedDim = 'PCA')
-sce <- slingshot(sce, clusterLabels = 'seurat_clusters', reducedDim = 'PCA')
-
-# Plot
-summary(sce$slingPseudotime_1)
-
-# Trajectory
-colors <- colorRampPalette(brewer.pal(11,'Spectral')[-6])(100)
-plotcol <- colors[cut(sce$slingPseudotime_1, breaks=100)]
-
-png(glue("49_trajectory_analysis/plot/{HH}_{clone}_slingshot.png"), res = 1000, width = 9000, height = 6000)
-
-plot(reducedDims(sce)$PCA, col = plotcol, pch=16, asp = 1, main = glue("{HH}: clone {clone}"))
-lines(SlingshotDataSet(sce), lwd=2, col='black')
-
-dev.off()
+# lin1 <- getLineages(sce, clusterLabel)
+lin1 <- getLineages(sce, 'seurat_clusters')
 
 # Isotype
 colors <- isotype_colors_custom
 plotcol <- colors[sce$c_call]
-
-png(glue("49_trajectory_analysis/plot/{HH}_{clone}_isotype.png"), res = 1000, width = 9000, height = 6000)
 plot(reducedDims(sce)$PCA, col = plotcol, pch=16, asp = 1, main = glue("{HH}: clone {clone}"))
-lines(SlingshotDataSet(sce), lwd=2, col='black')
+lines(SlingshotDataSet(lin1), lwd=2, col='black')
 
 present <- unique(sce$c_call)
 legend("topright",
@@ -144,16 +236,11 @@ legend("topright",
        bty = "n",
        cex = 0.8)
 
-dev.off()
-
 # Cell type
 colors <- L1_colors
 plotcol <- colors[sce$L1_annotation]
-
-png(glue("49_trajectory_analysis/plot/{HH}_{clone}_L1.png"), res = 1000, width = 9000, height = 6000)
-
 plot(reducedDims(sce)$PCA, col = plotcol, pch=16, asp = 1, main = glue("{HH}: clone {clone}"))
-lines(SlingshotDataSet(sce), lwd=2, col='black')
+lines(SlingshotDataSet(lin1), lwd=2, col='black')
 
 present <- unique(sce$L1_annotation)
 legend("topright",
@@ -162,33 +249,6 @@ legend("topright",
        pch = 16,
        bty = "n",
        cex = 0.8)
-
-dev.off()
-
-# sample
-colors <- sample_clean_plot_colors
-plotcol <- colors[sce$sample_clean_plot]
-
-png(glue("49_trajectory_analysis/plot/{HH}_{clone}_sample.png"), res = 1000, width = 9000, height = 6000)
-
-plot(reducedDims(sce)$PCA, col = plotcol, pch=16, asp = 1, main = glue("{HH}: clone {clone}"))
-lines(SlingshotDataSet(sce), lwd=2, col='black')
-
-present <- unique(sce$sample_clean_plot)
-legend("topright",
-       legend = names(colors[present]),
-       col = colors[present],
-       pch = 16,
-       bty = "n",
-       cex = 0.8)
-
-dev.off()
-
-
-
-
-
-
 
 
 
