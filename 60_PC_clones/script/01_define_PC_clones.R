83597e4e884b002884f6400800b92b7f007ae881
helenewegener1 <helene.wegener@gmail.com>
2026-07-10 07:43:57 +0200

    PC clone analysis begun

---
+++ 60_PC_clones/script/01_define_PC_clones.R
@@ -0,0 +1,118 @@
+library(glue)
+library(tidyverse)
+library(Biostrings) 
+
+# ------------------------------------------------------------------------------
+# Load data
+# ------------------------------------------------------------------------------
+
+rds_files <- list.files("45_immcantation/out/rds") 
+resolve_LC_files <- grep("resolve_LC_3_definitions", rds_files, value = TRUE)
+
+patients <- lapply(resolve_LC_files, function(x) str_split_i(x, "_", 1)) %>% unlist()
+patients
+
+# ------------------------------------------------------------------------------
+# Get data
+# ------------------------------------------------------------------------------
+
+# Load both patients
+df_both <- lapply(patients, function(HH) {
+  readRDS(glue("45_immcantation/out/rds/{HH}_resolve_LC_3_definitions.rds")) %>%
+    filter(
+      locus == "IGH"
+      # !is.na(manual_ADT_full_ID)
+    ) %>%
+    mutate(patient = HH)
+}) %>% setNames(patients)
+
+# df_both$clone_subgroup_id_90_similarity
+
+# ------------------------------------------------------------------------------
+# Define top PC clones 
+# ------------------------------------------------------------------------------
+
+PC_clones <- lapply(patients, function(HH){
+  
+  df_both[[HH]] %>% 
+    filter(
+      L1_annotation == "PCs"
+    ) %>% 
+    count(clone_subgroup_id_90_similarity, sort = TRUE) %>% 
+    head(10) %>% 
+    pull(clone_subgroup_id_90_similarity)
+  
+}) %>% setNames(patients)
+
+
+# ------------------------------------------------------------------------------
+# Define clones 
+# ------------------------------------------------------------------------------
+
+clone_nrs <- 1:10
+
+for (HH in patients){
+  
+  # HH <- "HH117"
+  HH_spec_clones_vj <- df_both[[HH]]
+  
+  for (clone_nr in clone_nrs){
+    
+    # Get top "clone_nr" clone from given sample
+    # clone_nr <- 1
+    
+    # rank those clones by total size (all cell types) and take top 10
+    clone <- PC_clones[[HH]][[clone_nr]]
+    
+    # Extract sequences of clone, each's abundance and the germline sequence (GL)
+    seqs <- HH_spec_clones_vj %>% filter(clone_subgroup_id_90_similarity == clone & locus == "IGH") %>% pull(sequence_alignment)
+    GL <- HH_spec_clones_vj %>% filter(clone_subgroup_id_90_similarity == clone & locus == "IGH") %>% count(germline_alignment_d_mask, sort = TRUE) %>% pull(germline_alignment_d_mask)
+    
+    # PHYLIP format no longer allows dots in sequence (ValueError when running gctree duplicate)
+    seqs <- seqs %>% str_replace_all("\\.", "-")
+    GL <- GL %>% str_replace_all("\\.", "-")
+    GL <- GL %>% str_replace_all("N", "-")
+    
+    # Make sure there's only one germline sequence
+    length(GL) == 1
+    
+    # Check length of seqeunce to make sure that they are aligned
+    map(seqs, nchar) %>% unique()
+    nchar(GL)
+    
+    # Convert sequence into fasta format and name each sequence by their abundance
+    seqs_fasta <- DNAStringSet(seqs)
+    names(seqs_fasta) <- paste0("sequence_", 1:length(seqs_fasta))
+    
+    # Convert germline sequence into fasta format and name the sequence "GL"
+    GL_fasta <- DNAStringSet(GL)
+    names(GL_fasta) <- "GL"
+    
+    # Add germline to rest 
+    final_fasta <- c(seqs_fasta, GL_fasta)
+    
+    # Align sequence - Skip this since sequences are already aligned
+    ## Run alignment (ClustalW, ClustalOmega, or MUSCLE available)
+    # alignment <- msa(final_fasta, method = "ClustalOmega")
+    ## Convert to DNAStringSet for downstream use
+    # alignment_dna <- as(alignment, "DNAStringSet")
+    
+    # Export as FASTA file
+    outdir <- glue("60_PC_clones/fasta")
+    dir.create(outdir, recursive = TRUE, showWarnings = FALSE)
+    writeXStringSet(final_fasta, filepath = glue("{outdir}/{HH}_clone_nr_{clone_nr}_clone_{clone}.fasta"))
+    
+  }
+  
+}
+
+
+
+  
+
+  
+
+
+
+
+
